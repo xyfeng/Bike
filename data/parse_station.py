4b6e5ba2e4fa43d2bc450bceb33a5323ef259580
XY <fxy0111@gmail.com>
2014-01-21 15:16:30 -0500

    add station size and initial animation

--- data/parse_station.py
+++ data/parse_station.py
@@ -1,13 +1,19 @@
 import json, urllib2, time
 
 stations = json.load(open('citibike.json'))['stationBeanList']
-neighborhoods = json.load(open('neighborhood.json'))
+# neighborhoods = json.load(open('neighborhood.json'))
 stationJSON = []
 
 # Create table
 # stationCursor.execute('''CREATE TABLE stations (id INTEGER PRIMARY KEY, name TEXT, lat REAL, lng REAL, bikes INTEGER, postal INTEGER, district TEXT)''')
 
+min = 100
+max = 0
+
 for station in stations:
+	if station['statusKey'] == 3:
+		continue
+
 	id = station['id']
 	# print id
 	name = station['stationName']
@@ -15,33 +21,44 @@ for station in stations:
 	lng = station['longitude']
 	bikes = station['totalDocks']
 
+
+	# Get min and max of bikes
+	if bikes > max:
+		max = bikes
+
+	if bikes < min:
+		min = bikes
+
+
 	# Get Neighborhood
-	bboxSize = 2
-	for n in neighborhoods:
-		bbox = n['bbox']
-		if lat < bbox[3] and lat > bbox[1] and lng < bbox[2] and lng > bbox[0]:
-			newSize = bbox[2] + bbox[3] - bbox[0] - bbox[1]
-			if newSize < bboxSize:
-				bboxSize = newSize
-				print n['name']
-				neighborhood = n['name']
+	# bboxSize = 2
+	# for n in neighborhoods:
+	# 	bbox = n['bbox']
+	# 	if lat < bbox[3] and lat > bbox[1] and lng < bbox[2] and lng > bbox[0]:
+	# 		newSize = bbox[2] + bbox[3] - bbox[0] - bbox[1]
+	# 		if newSize < bboxSize:
+	# 			bboxSize = newSize
+	# 			print n['name']
+	# 			neighborhood = n['name']
 
-	# Get Postal Code
-	postalURL = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(lat) + "," + str(lng) + "&sensor=false"
-	postalResponse = json.load(urllib2.urlopen(postalURL))
-	postal = int(postalResponse['results'][0]['address_components'][-1]['short_name'])
-	print postal
+	# # Get Postal Code
+	# postalURL = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(lat) + "," + str(lng) + "&sensor=false"
+	# postalResponse = json.load(urllib2.urlopen(postalURL))
+	# postal = int(postalResponse['results'][0]['address_components'][-1]['short_name'])
+	# print postal
 
-	one = {}
-	one['sid'] = id
-	one['name'] = name
-	one['lat'] = lat
-	one['lng'] = lng
-	one['bikes'] = bikes
-	one['postal'] = postal
-	one['neighborhood'] = neighborhood
-	stationJSON.append(one)
+	# one = {}
+	# one['sid'] = id
+	# one['name'] = name
+	# one['lat'] = lat
+	# one['lng'] = lng
+	# one['bikes'] = bikes
+	# one['postal'] = postal
+	# one['neighborhood'] = neighborhood
+	# stationJSON.append(one)
 
-with open('station.json', 'w') as outfile:
-  json.dump(stationJSON, outfile)
+# with open('station.json', 'w') as outfile:
+#   json.dump(stationJSON, outfile)
 
+print 'min number: ' + str(min)
+print 'max number: ' + str(max)
--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 $(function(){
+
 	//DRAW SETUP
 	var two = new Two({
-        autostart: true,
+        autostart: false,
 		fullscreen: true
 	}).appendTo(document.body);
 
 	//ZOOM & DRAG SETUP
 	var zui = new ZUI(two);
-	zui.addLimits(0.06, 8);
+	zui.addLimits(0.5, 5);
 
 	var $stage = $(two.renderer.domElement);
 	var dragStart = {};
@@ -40,6 +41,17 @@ $(function(){
 			.bind('mouseup', dragEnd);
 	});
 
+	//GUI SETUP
+	function Settings(){
+		this.strokeWidth = 0.0;
+	}
+	var settings = new Settings();
+	var gui = new dat.GUI();
+	var f1 = gui.addFolder('style');
+	f1.add(settings, 'strokeWidth', 0, 1);
+	f1.closed = true;
+
+	//OBJECTS
 	function StationDot(dot, id, name, bikes, postal, neighborhood){
 		this.dot = dot;
 		this.id = id;
@@ -48,9 +60,29 @@ $(function(){
 		this.postal = postal;
 		this.neighborhood = neighborhood;
 	}
-	var stations = new Array();
 
-	//Interaction
+	//FUNCTIONS
+
+	//map projection
+	var dest = 'PROJCS["NAD83 / Massachusetts Mainland",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",42.68333333333333],PARAMETER["standard_parallel_2",41.71666666666667],PARAMETER["latitude_of_origin",41],PARAMETER["central_meridian",-71.5],PARAMETER["false_easting",200000],PARAMETER["false_northing",750000],AUTHORITY["EPSG","26986"],AXIS["X",EAST],AXIS["Y",NORTH]]';
+	var center = proj4(dest, [-73.9770,40.7214]);
+	function getScreenPos(lat, lng){
+		var pos = {};
+
+		var point = proj4(dest,[lng,lat]);
+
+		pos.x = two.width / 2 + (point[0] - center[0])/10;
+		pos.y = two.height / 2 - (point[1] - center[1])/10;
+
+		return pos;
+	}
+
+	function downloadStations(){
+		$.getJSON("http://bike.parseapp.com/getstations", function(data) {
+			stationData = data;
+		});
+	}
+
 	function addInteractivity(station) {
 		$(station.dot._renderer.elem)
 			.css({
@@ -66,64 +98,57 @@ $(function(){
 			})
 	}
 
-	//GUI SETUP
-	function Settings(){
-		this.strokeWidth = 0.0;
-	}
-	var settings = new Settings();
-	var gui = new dat.GUI();
-	var f1 = gui.addFolder('style');
-	f1.add(settings, 'strokeWidth', 0, 1);
-	f1.closed = false;
 
-	//MAP PROJECTION
-	var dest = 'PROJCS["NAD83 / Massachusetts Mainland",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",42.68333333333333],PARAMETER["standard_parallel_2",41.71666666666667],PARAMETER["latitude_of_origin",41],PARAMETER["central_meridian",-71.5],PARAMETER["false_easting",200000],PARAMETER["false_northing",750000],AUTHORITY["EPSG","26986"],AXIS["X",EAST],AXIS["Y",NORTH]]';
-	var center = proj4(dest, [-73.9770,40.7214]);
-	function getScreenPos(lat, lng){
-		var pos = {};
 
-		var point = proj4(dest,[lng,lat]);
+	
+	//GLOBAL
+	var bikesNumMin = 12;
+	var bikesNumMax = 67;
+	var stationData = [];
+	var stations = new Array();
 
-		pos.x = two.width / 2 + (point[0] - center[0])/10;
-		pos.y = two.height / 2 - (point[1] - center[1])/10;
 
-		return pos;
-	}
 
-	$.getJSON("http://bike.parseapp.com/getstations", function(data) {
-		$.each( data, function( i, d ) {
+	//DRAW UPDATE
+	function updateStations(frameCount) {
+		if (stationData.length == 0) {
+			this.startFrame = frameCount;
+		}
+		else if(stations.length < stationData.length){
+			var i = frameCount - this.startFrame;
+			var d = stationData[i];
 			var pos = getScreenPos(d['lat'], d['lng']);
-			var dot = two.makeCircle(pos.x, pos.y, 2);
-			dot.fill = '#999';
+			var dot = two.makeCircle(pos.x, pos.y, 2 + 4 * (d['bikes'] - bikesNumMin) / (bikesNumMax - bikesNumMin) );
+			dot.fill = '#000';
 			dot.stroke = 'black';
 			dot.linewidth = 0.0;
 			
 			stations[i] = new StationDot(dot, d['sid'], d['name'], d['bikes'], d['postal'], d['neighborhood']);
-			two.update();
-
 			addInteractivity(stations[i]);
-		});
-		two.bind('update', updateTwo).play();
-	});
-
-	//DRAW UPDATE
-	function showRoutes(sid) {
-		$.getJSON("http://bike.parseapp.com/getroutes?start="+sid.toString(), function(data) {
-			$.each( data, function( i, d ) {
-				var points = [];
-				$.each ( d['points'], function (j, p) {
-					var pos = getScreenPos(p[1], p[0]);
-					points.push(new Two.Anchor(pos.x,pos.y));
-				});
-				var polygon = two.makePolygon(points, true);
-				polygon.noFill();
-				two.update();
-			});
-		});
-	}
-	function updateTwo(frameCount) {
-		for(var i = 0; i < stations.length; i ++){
-			stations[i].dot.linewidth = settings.strokeWidth;
 		}
 	}
+
+	//RUN 
+	downloadStations();
+	two.bind('update', updateStations);
+	two.play();
+
+	// for(var i = 0; i < stations.length; i ++){
+	// 	stations[i].dot.linewidth = settings.strokeWidth;
+	// }
+
+	// function showRoutes(sid) {
+	// 	$.getJSON("http://bike.parseapp.com/getroutes?start="+sid.toString(), function(data) {
+	// 		$.each( data, function( i, d ) {
+	// 			var points = [];
+	// 			$.each ( d['points'], function (j, p) {
+	// 				var pos = getScreenPos(p[1], p[0]);
+	// 				points.push(new Two.Anchor(pos.x,pos.y));
+	// 			});
+	// 			var polygon = two.makePolygon(points, true);
+	// 			polygon.noFill();
+	// 			two.update();
+	// 		});
+	// 	});
+	// }
 });
